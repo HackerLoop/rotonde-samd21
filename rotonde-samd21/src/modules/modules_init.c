c3337742f4759fbe3a8694c7019349c2ba6409cb
vitaminwater <stantin04@gmail.com>
2016-01-27 18:43:57 -0800

    removed template

--- rotonde-samd21/src/modules/modules_init.c
+++ rotonde-samd21/src/modules/modules_init.c
@@ -3,14 +3,10 @@
  *
  * Created: 20/01/2016 18:19:08
  *  Author: stant
- */ 
+ */
 
 #include "modules/modules_init.h"
 
-#include "modules/modules/template.h"
-
-#include "modules/modules/pingpong.h"
-
 void init_modules(void) {
-	Pinpong_module_init();
+
 }
